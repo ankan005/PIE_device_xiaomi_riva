64af1122291e9c0f844950cd36b7a77070b80549
sunnyraj84348 <sunnyraj84348@gmail.com>
2018-06-07 16:28:49 +0000

    AEX Bringup

--- aosp_riva.mk
+++ aosp_riva.mk
@@ -19,8 +19,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AEX stuff
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit from riva device
 $(call inherit-product, device/xiaomi/riva/device.mk)
@@ -29,7 +29,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := riva
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 5A
-PRODUCT_NAME := lineage_riva
+PRODUCT_NAME := aosp_riva
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
