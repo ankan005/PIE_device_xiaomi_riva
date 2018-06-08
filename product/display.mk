1d26e7df59e6f94d9c6239439a296f9ce8ce6b6e
sunnyraj84348 <sunnyraj84348@gmail.com>
2018-06-08 15:23:08 +0000

    Removed Unnecessary Stuffs

--- product/display.mk
+++ product/display.mk
@@ -20,8 +20,10 @@ PRODUCT_PACKAGES += \
     copybit.msm8937 \
     gralloc.msm8937 \
     hwcomposer.msm8937 \
+    liboverlay \
     libdisplayconfig \
     libqdMetaData.system \
+    libjni_livedisplay \
     libtinyxml \
     memtrack.msm8937
 
