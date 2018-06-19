c5e5271b953d5349d6bb960f60c1fa71420458c5
sunnyraj84348 <sunnyraj84348@gmail.com>
2018-06-19 03:02:25 +0000

    ;_;

--- lineage.mk
+++ lineage.mk
@@ -38,7 +38,7 @@ TARGET_VENDOR_PRODUCT_NAME := riva
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="riva" \
     PRODUCT_NAME="riva" \
-    PRIVATE_BUILD_DESC="riva-user 7.1.2 N2G47H V9.2.6.0.NCCMIEK release-keys"
+    PRIVATE_BUILD_DESC="rolex-user 7.1.2 N2G47H V9.2.6.0.NCCMIEK release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Xiaomi/rolex/rolex:7.1.2/N2G47H/V9.2.6.0.NCCMIEK:user/release-keys"
